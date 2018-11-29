08494fbddb3071f48a96226965341a650d4bd60c
Daniel Mitre <daniel.mitre@ccc.ufcg.edu.br>
2018-11-29 11:39:50 -0300

    Improved

--- doqueru.cpp
+++ doqueru.cpp
@@ -3,13 +3,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <sched.h>
+#include <string.h>
 
 using namespace std;
 
+#define MOUNT_DIR "./doquerinho's_shell"	//checar pronuncia
 
 //ps fc pra visualizar
 void fork_demostration() {
-
   pid_t pid;
   pid = fork();
 
@@ -18,7 +21,6 @@ void fork_demostration() {
   } else {
     printf("Hello, World! ( parent ) \n");
   }
-
 }
 
 void updated_fork() {
@@ -26,7 +28,7 @@ void updated_fork() {
   if ((pid = fork()) == 0) {
     printf("Hello !! ( child ) \n");
   }
-    printf("Hello, World! ( parent ) \n");
+  printf("Hello, World! ( parent ) \n");
 }
 
 void exec_demo(char** argv) {
@@ -41,7 +43,7 @@ void fork_and_exec(char** argv) {
   }
   wait(status);
   printf("===============\n");
-  printf("Now the father!\n");
+  printf("Now the father! My child has gone :(\n");
   printf("===============\n");
   execvp("./hello_worldo", argv+1);
 }
@@ -61,14 +63,107 @@ void fork_and_exec_env(char** argv) {
   execvp("./hello_worldo", argv+1);
 }
 
-
-int main(int argc, char** argv) {
-  //fork_demostration();
-  //updated_fork();
-  //exec_demo(argv);
-  //fork_and_exec(argv);
-  fork_and_exec_env(argv);
-  return 0;
+void safe_unshare(int flags) {
+	if (0 == unshare(flags)) {
+		printf("unshare worked!\n");
+	} else {
+		printf("unshare has failed! :( [errno=%d]\n", errno);
+	}
 }
 
-//unshare for fork
+void safe_sethostname() {
+	char new_name[] = "carl";
+	if (0 == sethostname(new_name, strlen(new_name))) {
+		printf("sethostname worked!\n");
+	} else {
+		printf("sethostname has failed! :( [errno=%d]\n", errno);
+	}
+}
+
+void fork_and_exec_env_unshareuts(char** argv) {
+  int* status;
+  pid_t pid;
+  int uns_flags = CLONE_NEWUTS;
+  if ((pid = fork()) == 0) {
+    clearenv();
+	safe_unshare(uns_flags);
+	safe_sethostname();
+    execvp("./hello_worldo", argv+1);
+  }
+  wait(status);
+  printf("===============\n");
+  printf("Now the father!\n");
+  printf("===============\n");
+  execvp("./hello_worldo", argv+1);
+}
+
+void fork_and_exec_env_unshareutspid(char** argv) {
+  int* status;
+  pid_t pid;
+  int uns_flags = CLONE_NEWUTS | CLONE_NEWPID;
+  if ((pid = fork()) == 0) {
+    clearenv();
+	safe_unshare(uns_flags);
+	safe_sethostname();
+    execvp("./hello_worldo", argv+1);
+  }
+  wait(status);
+  printf("===============\n");
+  printf("Now the father!\n");
+  printf("===============\n");
+  execvp("./hello_worldo", argv+1);
+}
+
+void download_alpine();
+
+void setup_new_env() {
+  clearenv();
+  setenv("TERM", "xterm-256color", 0);
+  setenv("PATH", "/bin/:/sbin/:usr/bin:/usr/sbin", 0);	//remember to explain paths
+}
+
+void fork_and_exec_env_unshareutspid_chdir(char** argv) {
+  int* status;
+  pid_t pid;
+  int uns_flags = CLONE_NEWUTS | CLONE_NEWPID;
+  if ((pid = fork()) == 0) {
+    setup_new_env();
+	safe_unshare(uns_flags);
+	safe_sethostname();
+	//remember alpine
+	chroot(MOUNT_DIR);
+  	chdir("/");
+    execvp("./hello_worldo", argv+1);
+  }
+  wait(status);
+  printf("===============\n");
+  printf("Now the father!\n");
+  printf("===============\n");
+  execvp("./hello_worldo", argv+1);
+}
+
+void fork_and_exec_env_unshareutspid_chdir_pivot(char** argv) {
+  int* status;
+  pid_t pid;
+  int uns_flags = CLONE_NEWUTS | CLONE_NEWPID;
+  if ((pid = fork()) == 0) {
+    setup_new_env();
+	safe_unshare(uns_flags);
+	safe_sethostname();
+	//remember alpine
+	chroot(MOUNT_DIR);
+  	chdir("/");
+    execvp("./hello_worldo", argv+1);
+  }
+  wait(status);
+  printf("===============\n");
+  printf("Now the father!\n");
+  printf("===============\n");
+  execvp("./hello_worldo", argv+1);
+}
+
+
+int main(int argc, char** argv) {
+  fork_and_exec_env_unshareutspid_chdir(argv);
+  return 0;
+}
--- hello_worldo.cpp
+++ hello_worldo.cpp
@@ -2,7 +2,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
 using namespace std;
 
 
@@ -20,14 +19,13 @@ using namespace std;
 
 void test_file() {
 	FILE *file;
-	file = fopen ("test_file.txt","r");
-	if (file!=NULL) {
+	file = fopen("test_file.txt","r");
+	if (file != NULL) {
 		printf("File found!\n");
 		fclose (file);
   	} else {
   		printf("File not found :(\n");
   	}
-
 }
 
 void pid_test() {
@@ -36,20 +34,25 @@ void pid_test() {
 }
 
 void env_variable() {
-	char* env_var_name = "HOME";
+	char env_var_name[] = "HOME";
 	char* var = getenv(env_var_name);
-
 	if (var != NULL)
 		printf("Variable %s found : %s\n",env_var_name,var);
-	else 
+	else
 		printf("Variable not found :(\n");
 }
 
-
+void test_uts() {
+	const size_t max_len = 100;
+	char nome[max_len];
+	gethostname(nome, max_len);
+	printf("[Hostname]=%s\n", nome);
+}
 
 int main() {
 	test_file();
 	pid_test();
 	env_variable();
+	test_uts();
 	return 0;
 }
