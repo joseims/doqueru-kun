5c5f5b2b8ad50a3d64c33cb2f6ca274d20dae7ca
José Ignácio <jose.schmid@ccc.ufcg.edu.br>
2018-12-19 18:33:39 -0300

    ?????????? não da erro mas também não vai

--- doqueru.cpp
+++ doqueru.cpp
@@ -52,7 +52,7 @@ void setup_new_env() {
 void ls() {
   int status;
   if (fork() == 0) {
-    char* args_ls[] = {"./doquerinhos_shell/bin/ls", "-la", NULL};
+    char* args_ls[] = {"/bin/ls", "-la", NULL};
     execv(args_ls[0], args_ls);
     printf("%s %s exited with errorno=%d\n", args_ls[0], args_ls[1], errno);
   }
@@ -86,89 +86,44 @@ int copy(char source[], char dest[]) {
   return system(c);
 }
 
-void fork_and_exec_env_unshareutspid_chdir_pivot(char** argv) {
+void doqueru(char** argv) {
   int status;
   pid_t pid;
   int uns_flags = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS;
 
-  if ((pid = fork()) == 0) {
-    setup_new_env();
-  	safe_unshare(uns_flags);
-  	safe_sethostname();
+   if ((pid = fork()) == 0) {
+      setup_new_env();
+      safe_unshare(uns_flags);
+      safe_sethostname();
 
-    /* Enter the mount and user namespaces. Note that in some cases (e.g., RHEL
-       6.8), this will succeed even though the userns is not created. In that
-       case, the following mount(2) will fail with EPERM. */
+      TRY(mount("none", "/", NULL, MS_REC|MS_PRIVATE, NULL));
 
+      TRY(mount(MOUNT_DIR, MOUNT_DIR, NULL,
+                  MS_BIND | MS_PRIVATE, NULL));
 
+      chdir(MOUNT_DIR);
 
-    /* Ensure that our image directory exists. It doesn't really matter what's
-       in it. */
-    if (mkdir("/tmp/newroot", 0755) && errno != EEXIST)
-       TRY (errno);
-
-       ls();
-    int a = copy("/home/dokeru/workspace/doqueru-kun/doquerinhos_shell","/tmp/newroot");
-		printf("copy return(%d) errno(%d)\n",a,errno );
+      /* Make a place for the old (intermediate) root filesystem to land. */
+      if (mkdir("oldroot", 0755) && errno != EEXIST)
+        TRY (errno);
 
 
-
-    /* Claim the image for our namespace by recursively bind-mounting it over
-       itself. This standard trick avoids conditions 1 and 2. */
-    TRY (mount("/tmp/newroot", "/tmp/newroot", NULL,
-               MS_REC | MS_BIND | MS_PRIVATE, NULL));
-
-    /* The next few calls deal with condition 3. The solution is to overmount
-       the root filesystem with literally anything else. We use the parent of
-       the image, /tmp. This doesn't hurt if / is not a rootfs, so we always do
-       it for simplicity. */
-
-    /* Claim /tmp for our namespace. You would think that because /tmp contains
-       /tmp/newroot and it's a recursive bind mount, we could claim both in the
-       same call. But, this causes pivot_root(2) to fail later with EBUSY. */
-    TRY (mount("/tmp", "/tmp", NULL, MS_REC | MS_BIND | MS_PRIVATE, NULL));
-
-    /* chdir to /tmp. This moves the process' special "." pointer to
-       the soon-to-be root filesystem. Otherwise, it will keep pointing to the
-       overmounted root. See the e-mail at the end of:
-       https://git.busybox.net/busybox/tree/util-linux/switch_root.c?h=1_24_2 */
-    TRY (chdir("/tmp"));
-
-    /* Move /tmp to /. (One could use this to directly enter the image,
-       avoiding pivot_root(2) altogether. However, there are ways to remove all
-       active references to the root filesystem. Then, the image could be
-       unmounted, exposing the old root filesystem underneath. While
-       Charliecloud does not claim a strong isolation boundary, we do want to
-       make activating the UDSS irreversible.) */
-    TRY (mount("/tmp", "/", NULL, MS_MOVE, NULL));
-
-    /* Move the "/" special pointer to the new root filesystem, for the reasons
-       above. (Similar reasoning applies for why we don't use chroot(2) to
-       directly activate the UDSS.) */
-    TRY (chroot("."));
-
-    /* Make a place for the old (intermediate) root filesystem to land. */
-    if (mkdir("/newroot/oldroot", 0755) && errno != EEXIST)
-       TRY (errno);
-
-    /* Re-mount the image read-only. */
-    TRY (mount(NULL, "/newroot", NULL, MS_REMOUNT | MS_BIND | MS_RDONLY, NULL));
-
-    /* Finally, make our "real" newroot into the root filesystem. */
-    TRY (chdir("/newroot"));
-    TRY (syscall(SYS_pivot_root, "/newroot", "/newroot/oldroot"));
-    TRY (chroot("."));
-
-    /* Unmount the old filesystem and it's gone for good. */
-    TRY (umount2("/oldroot", MNT_DETACH));
+      TRY(pivot_root(".", "./oldroot"));
 
 
-    printf("===============\n");
-    printf("Running child! pid[%d]\n", pid);
-    TRY(execvp("/bin/bash", argv + 1));
-    printf("child keep running error[%d]\n", errno);
-    exit(1);
-  }
+      //MOUNT PROC ETC
+      TRY(mount("proc","./proc","proc",NULL,NULL));
+
+
+      /* Unmount the old filesystem and it's gone for good. */
+      TRY (umount2("./oldroot", MNT_DETACH));
+      rmdir("./oldroot");
+
+        printf("===============\n");
+        printf("Running child! pid[%d]\n", pid);
+        TRY(execvp("/bin/sh", argv + 1));
+        printf("child keep running error[%d]\n", errno);
+   }
   wait(&status);
   printf("===============\n");
   printf("Now the father! pid[%d]\n", pid);
@@ -178,6 +133,6 @@ void fork_and_exec_env_unshareutspid_chdir_pivot(char** argv) {
 
 
 int main(int argc, char** argv) {
-  fork_and_exec_env_unshareutspid_chdir_pivot(argv);
+  doqueru(argv);
   return 0;
 }
