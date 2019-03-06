e8c5ba74602e8b9441104de09f2a0f3863f22e99
Daniel Mitre <daniel.mitre@ccc.ufcg.edu.br>
2019-03-06 02:03:19 -0300

    added uts isolation step

--- steps/README.md
+++ steps/README.md
@@ -1,3 +1,4 @@
 1. fork.cpp
 2. exec.cpp + test.cpp
 3. defensive.cpp
+4. uts.cpp
--- steps/uts.cpp
+++ steps/uts.cpp
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <cstring>
+#include <sched.h>
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif // _GNU_SOURCE
 
 #define STRINGIZE(x) #x
 
@@ -34,12 +39,21 @@ bool _assert(bool condition, const char msg_onerror[], int line, bool is_syscall
     return true;
 }
 
+void unsharenamespaces() {
+    const int UNSHARE_FLAGS = CLONE_NEWUTS;
+    const char* hostname = "doqueru";
+
+    ASSERTMSG(-1 != unshare(UNSHARE_FLAGS), "unshare has failed");
+    ASSERTMSG(-1 != sethostname(hostname, strlen(hostname)), "sethostname has failed");
+}
+
 void doqueru(char* exec_path, char** argv) {
     pid_t child_pid = fork();
-
     ASSERTMSG(-1 != child_pid, "fork has failed");
 
     if (child_pid == 0) {
+        unsharenamespaces();
+        
         ASSERTMSG(-1 != execvp(exec_path, argv), "exec has failed");
     }
 }
